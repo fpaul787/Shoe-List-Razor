f2269d15ccc0628830c21918f55f29c60d466cf0
Frantz Paul <fpaul787@gmail.com>
2020-10-21 17:45:41 -0400

    Added Shoe Index page

---
+++ ShoeListRazor/Pages/ShoeList/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ShoeListRazor.Model;
+
+namespace ShoeListRazor.Pages.ShoeList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Shoe> Shoes { get; set; }
+
+        public async void OnGet()
+        {
+            // going to database and receing all of the shoes
+            Shoes = await _db.Shoe.ToListAsync();
+        }
+    }
+}
