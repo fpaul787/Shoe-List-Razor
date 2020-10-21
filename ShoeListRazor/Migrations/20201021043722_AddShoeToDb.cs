3efa22ed057783a3b1284088e8b5f5608f605968
Frantz Paul <fpaul787@gmail.com>
2020-10-21 00:40:15 -0400

    add db connection and migration

---
+++ ShoeListRazor/Migrations/20201021043722_AddShoeToDb.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShoeListRazor.Migrations
+{
+    public partial class AddShoeToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Shoe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Brand = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shoe", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Shoe");
+        }
+    }
+}
