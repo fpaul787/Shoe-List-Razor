7881f1b6e1a4124d3424442313df2a0f5f0518e4
Frantz Paul <fpaul787@gmail.com>
2020-10-20 23:29:39 -0400

    Added new model

---
+++ ShoeListRazor/Model/Shoe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoeListRazor.Model
+{
+    public class Shoe
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public string Brand { get; set; }
+    }
+}
