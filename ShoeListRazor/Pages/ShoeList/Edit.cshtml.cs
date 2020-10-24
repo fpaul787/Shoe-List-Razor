a1dfdbb25277d4cee9a58a6abbad6e3e355e886c
Frantz Paul <fpaul787@gmail.com>
2020-10-23 22:16:48 -0400

    Edit Book Get Handler

--- ShoeListRazor/Pages/ShoeList/Edit.cshtml.cs
+++ ShoeListRazor/Pages/ShoeList/Edit.cshtml.cs
@@ -1,16 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShoeListRazor.Model;
 
 namespace ShoeListRazor.Pages.ShoeList
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
         {
+            
+        }
+
+        [BindProperty]
+        public Shoe Shoe { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Shoe = await _db.Shoe.FindAsync(id);
         }
     }
 }
