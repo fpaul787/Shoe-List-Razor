7a596452057bde9ab0a3b7aaf55b0a93428a5042
Frantz Paul <fpaul787@gmail.com>
2020-11-09 23:54:03 -0500

    weird javascript stuff

---
+++ ShoeListRazor/wwwroot/js/shoeList.js
@@ -0,0 +1,32 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#DT_LOAD').DataTable({
+        "ajax": {
+            "url": "/api/book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "30%" },
+            { "data": "brand", "width": "30%" },
+            { "data": "isbn", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/ShoeList/Edit?id=${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px'>
+                                Edit
+                            </a>
+                        </div>
+                    `
+                }
+            }
+        ]
+    })
+}
